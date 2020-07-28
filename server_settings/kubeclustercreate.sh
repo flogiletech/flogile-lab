eb3f2c63fc3297a7fc03dc74658f7cbc3e3f9c47
Your Name <you@example.com>
2020-07-28 12:28:21 +0000

    helm issue

--- server_settings/kubeclustercreate.sh
+++ server_settings/kubeclustercreate.sh
@@ -20,9 +20,19 @@ setup_configuration()
    chown $(id -u):$(id -g) $HOME/.kube/config
 }
 install_dependent()
+{  
+   apt install -y nfs-common
+   track_error $? "nfs-common installation" 
+   rm /lib/systemd/system/nfs-common.service
+   systemctl daemon-reload
+   systemctl start nfs-common
+   track_error $? "nfs-common start"
+   systemctl status nfs-common
+}
+install_masterdependent()
 {
-   apt install -y helm
-   track_error $? "Helm installation" 
+   snap install helm --classic
+   track_error $? "Helm installation"
 }
 master_create()
 {
@@ -38,6 +48,7 @@ master_create()
    kubectl create -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
    track_error $? "Calico" 
    kubectl get nodes
+   install_masterdependent
    install_dependent
 }
 replicamaster_create()
@@ -51,6 +62,7 @@ replicamaster_create()
    track_error $? "Kube replica initialisation" 
    setup_configuration
    kubectl get nodes
+   install_masterdependent
    install_dependent
 }
 apply_configuration()
@@ -64,6 +76,18 @@ apply_configuration()
    cp -f common/keepalived.yaml /etc/kubernetes/manifests/keepalived.yaml
    cp -f common/haproxy.yaml /etc/kubernetes/manifests/haproxy.yaml
 }
+create_node()
+{
+   if [ -z $KUBE_VIP ] || [ -z $KUBE_TOKEN ] || [ -z $KUBE_SHA256 ] ;
+   then
+     log "Missing KUBE_VIP KUBE_TOKEN KUBE_SHA256"
+     exit 1
+   fi
+   kubeadm join $KUBE_VIP:6443 --token $KUBE_TOKEN --discovery-token-ca-cert-hash sha256:$KUBE_SHA256
+   track_error $? "Kube replica initialisation"
+   install_dependent
+
+}
 if [ -e kubeconf ];
 then
 	source kubeconf
@@ -72,9 +96,14 @@ else
 	exit 1
 fi
 
-if [ "$servername" = "master1" ] || [ "$servername" = "master2" ] || [ "$servername" = "master3" ] ;
+if [ "$servername" = "master1" ] || [ "$servername" = "master2" ] || [ "$servername" = "master3" ] || [ "$servername" = "node" ] ;
 then 
-   apply_configuration
+   if [ "$servername" = "node" ];	
+   then
+      log "setting not  needed"
+   else	   
+      apply_configuration
+   fi	   
 else 
    log "please provide proper value to the code"
    exit 1
@@ -84,6 +113,12 @@ if [ "$servername" = "master1" ];
 then 
    master_create
 else 
-   replicamaster_create
+   if [ "$servername" = "node" ];
+   then
+     create_node
+   else
+     replicamaster_create
+   fi
+   
 fi
 
